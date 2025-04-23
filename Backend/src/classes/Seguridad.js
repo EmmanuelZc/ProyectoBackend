59bdc62471434d6794bbfe49a72e4eb5f7d8798e
EmmanuelZc <103443577+EmmanuelZc@users.noreply.github.com>
2025-04-23 17:41:11 -0600

    prueba repo

--- Backend/src/classes/Seguridad.js
+++ Backend/src/classes/Seguridad.js
@@ -4,7 +4,7 @@ export default class Seguridad {
   static async hash(contrasena) {
     return bcrypt.hash(contrasena, 10);
   }
-
+//aaaaaaaaaaaaaaaaaaaaaaa
   static async compararContrasena(contrasena, hash) {
     return bcrypt.compare(contrasena, hash);
   }
